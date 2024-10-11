13685227bf0ea37dc993c93449cf83a0f927818e
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-10-11 16:28:57 +0300

    add to models model ShowTheme

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
