b401bf3dc11a1db6d2aa42a3087f71ff6693b44d
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-10-13 14:52:13 +0300

    add for api/views ShowThemeViewSet

--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+import csv
+from datetime import datetime
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import viewsets, status
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from api.models import ShowTheme, AstronomyShow, ShowSession, PlanetariumDome, Ticket, Reservation
+from api.serializers.planetarium_serializers import (
+    ShowThemeSerializer, AstronomyShowSerializer, ShowSessionSerializer,
+    ShowSessionListSerializer, ShowSessionRetrieveSerializer,
+    PlanetariumDomeSerializer, TicketSerializer, TicketListSerializer,
+    ReservationSerializer, ReservationCreateSerializer, TicketRetrieveSerializer
+)
+from api.validators import validate_show_time
 
-# Create your views here.
+class ShowThemeViewSet(viewsets.ModelViewSet):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
