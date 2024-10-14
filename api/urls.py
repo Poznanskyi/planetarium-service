a3a61398d38356e7b76b14f273e756d344ab93d1
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-10-14 18:46:15 +0300

    modified api/urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from api.views.planetarium_views import (
+from views import (
     ShowThemeViewSet,
     AstronomyShowViewSet,
     ShowSessionViewSet,
